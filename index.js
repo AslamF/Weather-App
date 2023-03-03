1f93b2a55ce332983fbf4b119201edadebbaf484
Fraz Aslam <faslam7646@gmail.com>
2023-03-02 22:58:46 -0500

    added styling and render

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const form = document.querySelector("form");
 const button = document.querySelector("button");
 const cityValue = document.querySelector("#city");
+const temperature = document.querySelector(".temperatureContainer");
+const locationName = document.querySelector(".locationContainer");
+const weatherType = document.querySelector(".weatherContainer");
+
+const array = [];
 
 form.addEventListener("submit", () => {
   event.preventDefault();
@@ -16,20 +21,36 @@ async function fetchAPI() {
         "&units=imperial&appid=b98c623e9d5f8d81b7ec45e5228c80b8"
     );
     const weatherData = await response.json();
-    console.log(weatherData.main.temp_max);
+    console.log(weatherData);
+
+    const temp = weatherData.main.temp;
+    const wind = weatherData.wind.speed;
+    const name = weatherData.name;
+    const weather = weatherData.weather[0].main;
+    const dtTime = new Date();
+
+    const object = new weatherObject(temp, wind, name, weather);
+    array.splice(0, array.length);
+    array.push(object);
+
+    console.log(object);
+    render();
   } catch (error) {
-    console.log("Error");
+    console.log(error);
   }
 }
-/*
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response.main.temp_max);
-      console.log(response.main.temp);
-      console.log(response.main.temp_min);
-      console.log(response.name);
-      console.log(response.wind.speed);
-    });
-*/
+
+function weatherObject(temp, wind, name, weather) {
+  this.temp = temp;
+  this.wind = wind;
+  this.name = name;
+  this.weather = weather;
+}
+
+function render() {
+  array.forEach((object) => {
+    temperature.textContent = object.temp;
+    locationName.textContent = object.name;
+    weatherType.textContent = object.weather;
+  });
+}
