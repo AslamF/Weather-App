9d0be37f588f9fa2a5396bc2c3ecb52ac8097f24
Fraz Aslam <faslam7646@gmail.com>
2023-03-07 17:57:40 -0500

    finalized everything, design, icons, font, and added gif api

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
 const visibility = document.querySelector(".visibility");
 const pressure = document.querySelector(".pressure");
+const cardContainer = document.querySelector(".cardContainer");
+const icons = document.querySelectorAll("#icon");
+const input = document.querySelector("input");
 
 const array = [];
 
@@ -25,7 +28,6 @@ async function fetchAPI() {
         "&units=imperial&appid=b98c623e9d5f8d81b7ec45e5228c80b8"
     );
     const weatherData = await response.json();
-    console.log(weatherData);
 
     const temp = weatherData.main.temp;
     const name = weatherData.name;
@@ -45,11 +47,27 @@ async function fetchAPI() {
     );
     array.splice(0, array.length);
     array.push(object);
+    input.classList.remove("invalid");
 
-    console.log(object);
-    render();
+    if (object.weather === "Clear") {
+      fetchClearGif();
+      render();
+    } else if (object.weather === "Clouds") {
+      fetchCloudsGif();
+      render();
+    } else if (object.weather === "Rain") {
+      fetchRainGif();
+      render();
+    } else if (object.weather === "Snow") {
+      fetchSnowGif();
+      render();
+    } else if (object.weather === "Windy") {
+      fetchWindyGif();
+      render();
+    }
   } catch (error) {
     console.log(error);
+    input.classList.add("invalid");
   }
 }
 
@@ -73,7 +91,10 @@ function weatherObject(
 
 function render() {
   array.forEach((object) => {
-    temperature.textContent = object.temp;
+    for (const icon of icons) {
+      icon.style.display = "flex";
+    }
+    temperature.textContent = object.temp + "\u2109";
     locationName.textContent = object.name;
     weatherType.textContent = object.weather;
     wind.textContent = object.wind + "m/s";
@@ -81,4 +102,70 @@ function render() {
     visibility.textContent = object.visibility + "km";
     pressure.textContent = object.pressure + "hPa";
   });
+}
+
+async function fetchClearGif() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/xUPGcM9CazM9H5KrEA?api_key=fSKL3IDzANShrL2jm9W1qMMYoFR78Hrr"
+    );
+    const gifdata = await response.json();
+    const clouds = gifdata.data.images.original.url;
+    cardContainer.style.backgroundImage = "url(" + clouds + ")";
+    console.log(gifdata);
+  } catch {
+    ("error");
+  }
+}
+
+async function fetchRainGif() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/26BGD4XaoPO3zTz9K?api_key=fSKL3IDzANShrL2jm9W1qMMYoFR78Hrr"
+    );
+    const gifdata = await response.json();
+    const rain = gifdata.data.images.original.url;
+    cardContainer.style.backgroundImage = "url(" + rain + ")";
+  } catch {
+    ("error");
+  }
+}
+
+async function fetchSnowGif() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/6Jgoyu5xFB5yE?api_key=fSKL3IDzANShrL2jm9W1qMMYoFR78Hrr"
+    );
+    const gifdata = await response.json();
+    const snow = gifdata.data.images.original.url;
+    cardContainer.style.backgroundImage = "url(" + snow + ")";
+  } catch {
+    ("error");
+  }
+}
+
+async function fetchCloudsGif() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/3o6EhOYMhOTANYgHMk?api_key=fSKL3IDzANShrL2jm9W1qMMYoFR78Hrr"
+    );
+    const gifdata = await response.json();
+    const clouds = gifdata.data.images.original.url;
+    cardContainer.style.backgroundImage = "url(" + clouds + ")";
+  } catch {
+    ("error");
+  }
+}
+
+async function fetchWindyGif() {
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/3ohfFxoiP6rbEZ4nrG?api_key=fSKL3IDzANShrL2jm9W1qMMYoFR78Hrr"
+    );
+    const gifdata = await response.json();
+    const windy = gifdata.data.images.original.url;
+    cardContainer.style.backgroundImage = "url(" + windy + ")";
+  } catch {
+    ("error");
+  }
 }
